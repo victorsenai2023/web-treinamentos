66a1acdae00d24c035f961a031489d9cfddb0cbf
Jose Victor <victorsenai2023@gmail.com>
2023-10-16 17:32:17 -0300

    Introdução Lógica

---
+++ js/operadores_aritimeticos.js
@@ -0,0 +1,19 @@
+var a, b, somar, subtrair, multiplicar, dividir, modular
+
+a = 5
+b = 5
+
+somar = a + b
+console.log('Somar ', somar)
+
+subtrair = a - b
+console.log('Subtrair ', subtrair)
+
+multiplicar = a * b
+console.log('Multiplicar ', multiplicar)
+
+dividir = a / b
+console.log('Dividir ', dividir)
+
+modular = a % b
+console.log('Modular ', modular)
---
+++ js/operadores_logicos.js
@@ -0,0 +1,13 @@
+var a, b 
+
+a = 2
+b = 10
+
+var eMaior = a > b
+var eIgual = a === b
+var eDiferente = a !== b
+
+console.log('É maior', eMaior)
+console.log('É igual', eIgual)
+console.log('É diferente', eDiferente)
+
