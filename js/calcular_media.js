74902558bae25f5f81810c2cb7da845c9ba9e0d9
Jose Victor <victorsenai2023@gmail.com>
2023-10-18 17:31:13 -0300

    se_senao

--- js/calcular_media.js
+++ js/calcular_media.js
@@ -9,9 +9,8 @@ nota4 = 6
 media = (nota1 + nota2 + nota3 + nota4) / 4
 
 if(media >=7) {
-    console.log(`O aluno ${nome} está aprovado com a média` ${media})
+    console.log(`O aluno ${nome} está aprovado com a média ${media}`)
 
-    else{
-        console.log(`O aluno ${nome} esta reporvado com a média` ${media})
+}else{
+        console.log(`O aluno ${nome} esta reporvado com a média ${media}`)
     }
-}
